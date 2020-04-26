b50da20c141004ee9203e06a5dabb540996bfa3b
Jussi Lemmetyinen <jussi.lemmetyinen@siili.fi>
2020-04-26 11:24:59 +0300

    Import amplify deps to app.js

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import AppContainer from './src/components/AppNavigation'
+import Amplify from "aws-amplify";
+import config from './aws-exports'
 
 export default () => (
     <AppContainer/>
